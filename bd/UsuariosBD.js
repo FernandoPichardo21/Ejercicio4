541264a1e2a77c7fcd49869f0464ebbcd8638b17
JesusPichardo <fernandopr.ti23@utsjr.edu.mx>
2024-07-08 08:29:40 -0600

    cambio en usuariosbd

--- bd/UsuariosBD.js
+++ bd/UsuariosBD.js
@@ -5,19 +5,19 @@ class UsuarioBD extends ConectarBD{
         super();//con super inicias las variables del constructor heredado en este caso conectarBD
     }
     async nuevoUsuario(usuario1){
-        const sql="insert into usuario values(null,'"+usuario1.nombre+"','"+usuario1.celular+"','"+usuario1.correo+"');";
+        const sql="insert into usuarios values(null,'"+usuario1.nombre+"','"+usuario1.celular+"','"+usuario1.correo+"');";
         try {
             await this.conectarMySql(); //intentar y esperar a que conecte mysql
             await this.conexion.execute(sql);
         console.log("Crea un nuevo usuario");
         await this.cerrarConexion();
     }catch (error) {
-        console.log("Error al agregar usuario "+error);
+        console.log("Error al agregar usuarios "+error);
         console.error(sql);    
         }
     }
     async mostrarUsuarios() {
-        const sql="SELECT * FROM usuario;";
+        const sql="SELECT * FROM usuarios;";
         try {
             await this.conectarMySql();
             const [UsuariosMySql]=await this.conexion.execute(sql);
@@ -31,7 +31,7 @@ class UsuarioBD extends ConectarBD{
         }
     }
     async usuarioId(id){
-        const sql="select * from usuario where idUsuario="+id+";";
+        const sql="select * from usuarios where idUsuario="+id+";";
         try {
             await this.conectarMySql();
             const [[usuario]]=await this.conexion.execute(sql);
@@ -44,7 +44,7 @@ class UsuarioBD extends ConectarBD{
         }
     }
     async EditarUsuario(usuario) {
-        const sql="update usuario set nombre='"+usuario.nombre+"' , celular= '"+usuario.celular+"' ,  correo='"+usuario.correo+"' where idUsuario='"+usuario.idUsuario+"' ;";
+        const sql="update usuarios set nombre='"+usuario.nombre+"' , celular= '"+usuario.celular+"' ,  correo='"+usuario.correo+"' where idUsuario='"+usuario.idUsuario+"' ;";
         const sql2=`update usuario set nombre='${usuario.nombre}' , celular='${usuario.celular}' ,  correo='${usuario.correo}' ;" ` 
         try {
             await this.conectarMySql();
@@ -58,7 +58,7 @@ class UsuarioBD extends ConectarBD{
     }
 
     async BorrarUsuario(idUsuario){
-        const sql="delete from usuario where idUsuario="+idUsuario+ ";";
+        const sql="delete from usuarios where idUsuario="+idUsuario+ ";";
     
     try {
         await this.conectarMySql();
